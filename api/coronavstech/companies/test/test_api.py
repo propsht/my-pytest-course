63c2cce52a3706b28f4c5cb00bbe328c8989f27b
Igor <igor.propsht@gmail.com>
2024-04-27 18:24:12 -0400

    step 20 test api get, post

--- api/coronavstech/companies/test/test_api.py
+++ api/coronavstech/companies/test/test_api.py
@@ -9,22 +9,25 @@ from api.coronavstech.companies.models import Company
 
 
 @pytest.mark.django_db
-class TestGetCompanies(TestCase):
+class BasicCompanyAPITestcase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.companies_url = reverse("companies-list")
 
+    def tearDown(self) -> None:
+        pass
+
+
+class TestGetCompanies(BasicCompanyAPITestcase):
     def test_zero_companies_should_return_empty_list(self) -> None:
-        client = Client()
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
 
     def test_one_company_exist_should_succeed(self) -> None:
-        client = Client()
         test_company = Company.objects.create(name="Amazon")
-        # the same url
-        companies_url = reverse("companies-list")
         # the same request
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         # print(response.content)
         response_content = json.loads(response.content)[0]
         self.assertEqual(response.status_code, 200)
@@ -34,3 +37,60 @@ class TestGetCompanies(TestCase):
         self.assertEqual(response_content.get("notes"), "")
 
         test_company.delete()
+
+
+class TestPostCompanies(BasicCompanyAPITestcase):
+    def test_create_company_without_arguments_should_fail(self) -> None:
+        response = self.client.post(path=self.companies_url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content), {"name": ["This field is required."]}
+        )
+
+    def test_create_exist_company_should_fail(self) -> None:
+        Company.objects.create(name="Netflix")
+        response = self.client.post(
+            path=self.companies_url,
+            data={
+                "name": "Netflix",
+            },
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content),
+            {"name": ["company with this name already exists."]},
+        )
+
+    def test_create_company_with_only_name_all_fields_should_be_default(self) -> None:
+
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name"},
+        )
+        self.assertEqual(response.status_code, 201)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content.get("name"), "test company name")
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+    def test_create_company_with_layoffs_status_should_succeed(self) -> None:
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name", "status": "Layoffs"},
+        )
+        self.assertEqual(response.status_code, 201)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content.get("name"), "test company name")
+        self.assertEqual(response_content.get("status"), "Layoffs")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+    def test_create_company_with_wrong_status_should_fail(self) -> None:
+        response = self.client.post(
+            path=self.companies_url,
+            data={"name": "test company name", "status": "WrongStatus"},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("WrongStatus", str(response.content))
+        self.assertIn("is not a valid choice.", str(response.content))
