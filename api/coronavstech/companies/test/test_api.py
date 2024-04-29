67b2a469a234b767d9e752d32621468ac0ddaae6
Igor <igor.propsht@gmail.com>
2024-04-29 17:09:09 -0400

    step 23 exceptions, test log warnings and info level

--- api/coronavstech/companies/test/test_api.py
+++ api/coronavstech/companies/test/test_api.py
@@ -103,10 +103,39 @@ class TestPostCompanies(BasicCompanyAPITestcase):
     def test_should_be_skipped(self) -> None:
         self.assertEqual(1, 2)
 
+
+#######################################################################################################
+# testing exceptions, logs
 def raise_covid19_exception() -> None:
     raise ValueError("CoronaVirus Exception")
     # pass
+
+
 def test_raise_covid19_exception_should_pass() -> None:
     with pytest.raises(ValueError) as e:
         raise_covid19_exception()
     assert "CoronaVirus Exception" == str(e.value)
+
+
+import logging
+
+logger = logging.getLogger("CORONA_LOGS")
+
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+# test info level logs
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
