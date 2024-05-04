66414b7234960e4179a1d0633e8a33804ded093f
Igor <igor.propsht@gmail.com>
2024-05-04 15:09:58 -0400

    step 34 fibonacci

--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -126,7 +126,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "johndoeson303@gmail.com"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_PASSWORD = "uiah yjqu uwsb fkls"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_FORM_EMAIL = EMAIL_HOST_USER
 
