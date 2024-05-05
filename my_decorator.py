1fcd053b51fe0888c87a5ae1902f1416931a3783
Igor <igor.propsht@gmail.com>
2024-05-05 11:58:56 -0400

    step 37 pipelines paralel and coverege

--- my_decorator.py
+++ my_decorator.py
@@ -19,18 +19,18 @@ def get_list_of_kwargs_for_function(
     return list_of_kwargs_for_function
 
 
-def my_parametrized(identifiers: str, values: List[Tuple[int, int]]) -> Decorator:
-    def my_parametrized_decorator(function: Callable) -> Callable:
-        def run_func_parametrized() -> None:
-            list_of_kwargs_for_function = get_list_of_kwargs_for_function(
-                identifiers=identifiers, values=values
-            )
-            for kwargs_for_function in list_of_kwargs_for_function:
-                print(
-                    f"calling function {function.__name__} with  {kwargs_for_function=}"
-                )
-                function(**kwargs_for_function)
-
-        return run_func_parametrized
-
-    return my_parametrized_decorator
+# def my_parametrized(identifiers: str, values: List[Tuple[int, int]]) -> Decorator:
+#     def my_parametrized_decorator(function: Callable) -> Callable:
+#         def run_func_parametrized() -> None:
+#             list_of_kwargs_for_function = get_list_of_kwargs_for_function(
+#                 identifiers=identifiers, values=values
+#             )
+#             for kwargs_for_function in list_of_kwargs_for_function:
+#                 print(
+#                     f"calling function {function.__name__} with  {kwargs_for_function=}"
+#                 )
+#                 function(**kwargs_for_function)
+#
+#         return run_func_parametrized
+#
+#     return my_parametrized_decorator
